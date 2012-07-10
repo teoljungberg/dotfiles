91577c8e688de0299b9c6875c090ccfcccf8fd88
Teo Ljungberg <teo.ljungberg@gmail.com>
2012-07-10 14:30:20 +0200

    added alias for git-pull

--- home/.zsh/applications/git.zsh
+++ home/.zsh/applications/git.zsh
@@ -14,14 +14,18 @@ alias gp='git push'
 compdef _git gp=git-push
 gdv() { git diff -w "$@" | view - }
 compdef _git gdv=git-diff
+
 alias gc='git commit -v'
 compdef _git gc=git-commit
 alias gca='git commit -v -a'
 compdef _git gca=git-commit
 alias gcm='git commit -m'
 compdef _git gcm=git-commit
+
 alias gco='git checkout'
 compdef _git gco=git-checkout
+alias gpl='git pull'
+compdef _git gpl=git-pull
 alias gb='git branch'
 compdef _git gb=git-branch
 alias ga='git add'
