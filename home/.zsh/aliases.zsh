0f5efc8ca5f49c1d3b29d33b95faadf186979693
Teo Ljungberg <teo.ljungberg@gmail.com>
2012-08-03 14:21:52 +0200

    structure

--- home/.zsh/aliases.zsh
+++ home/.zsh/aliases.zsh
@@ -7,11 +7,10 @@ alias mv="mv -i"
 alias cp="cp -i"
 alias py="python"
 alias rb="ruby"
-alias kilall='killall'
 alias reload='source ~/.zshrc'
 alias t='todo'
-alias lart='ls -1Art'
 alias fn='find . -name'
+alias kilall='killall'
 
 # ls
 alias ls="ls -GF"
@@ -19,15 +18,17 @@ alias l='ls -1'
 alias ll='ls -l'
 alias la='ls -A'
 alias lal="ls -Al"
+alias lart='ls -1Art'
 alias cl='clear'
-alias ..='cd ..'
-alias ...='cd ../..'
-alias ....='cd ../../../'
 alias tree='tree -ChFl'
 alias l1='tree --dirsfirst -ChFL 1'
 alias l2='tree --dirsfirst -ChFL 2'
 alias l3='tree --dirsfirst -ChFL 3'
 alias l4='tree --dirsfirst -ChFL 4'
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../../'
+
 
 # zmv ftw
 autoload -U zmv
