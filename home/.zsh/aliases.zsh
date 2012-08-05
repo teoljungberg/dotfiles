4d25108a4c605727f26e642a6e9e1d991bbd2764
Teo Ljungberg <teo.ljungberg@gmail.com>
2012-08-05 11:51:48 +0200

    editing config files rapidly procrastinating much?!

--- home/.zsh/aliases.zsh
+++ home/.zsh/aliases.zsh
@@ -7,19 +7,23 @@ alias mv="mv -i"
 alias cp="cp -i"
 alias py="python"
 alias rb="ruby"
+alias cl='clear'
 alias reload='source ~/.zshrc'
 alias t='todo'
-alias fn='find . -name'
 alias kilall='killall'
 
-# ls
+# Quick Editing
+alias ev="$EDITOR ~/.vimrc"
+alias et="$EDITOR ~/.tmux.conf"
+alias ez="$EDITOR ~/.zsh"
+
+# listing
 alias ls="ls -GF"
 alias l='ls -1'
 alias ll='ls -l'
 alias la='ls -A'
 alias lal="ls -Al"
 alias lart='ls -1Art'
-alias cl='clear'
 alias tree='tree -ChFl'
 alias l1='tree --dirsfirst -ChFL 1'
 alias l2='tree --dirsfirst -ChFL 2'
@@ -28,7 +32,6 @@ alias l4='tree --dirsfirst -ChFL 4'
 alias ..='cd ..'
 alias ...='cd ../..'
 alias ....='cd ../../../'
-
 
 # zmv ftw
 autoload -U zmv
