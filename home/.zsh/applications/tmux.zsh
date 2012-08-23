da8ae274e8b22175fe24b4830b24442750f1ac4d
Teo Ljungberg <teo.ljungberg@gmail.com>
2012-08-23 16:39:33 +0200

    naming

--- home/.zsh/applications/tmux.zsh
+++ home/.zsh/applications/tmux.zsh
@@ -7,5 +7,5 @@ case "$TERM" in
     ;;
 esac
 
-alias to_clip='tmux save-buffer -|pbcopy'
-alias from_clip='tmux set-buffer "$(pbpaste)"'
+alias tmux_to_clip='tmux save-buffer -| pbcopy'
+alias tmux_from_clip='tmux set-buffer "$(pbpaste)"'
