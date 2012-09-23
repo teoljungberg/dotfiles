7824cca1a55b574076136f8a115c62159fc611c3
Teo Ljungberg <teo.ljungberg@gmail.com>
2012-09-23 12:13:38 +0200

    Smartcase completion is the bees knees

--- home/.zsh/completion.zsh
+++ home/.zsh/completion.zsh
@@ -1,3 +1,6 @@
+# matches case insensitive for lowercase
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
+
 # pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
 zstyle ':completion:*' completer _complete _ignored
