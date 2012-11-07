bcdb7036592343e31d32394bc23656977ea9db5c
Teo Ljungberg <teo.ljungberg@gmail.com>
2012-11-07 17:17:05 +0100

    GO branch

--- home/.zsh/applications/git.zsh
+++ home/.zsh/applications/git.zsh
@@ -12,18 +12,15 @@ alias gd='git diff'
 compdef _git gd=git-diff
 alias gp='git push'
 compdef _git gp=git-push
-gdv() { git diff -w "$@" | view - }
-compdef _git gdv=git-diff
 
 alias gc='git commit -v'
-compdef _git gc=git-commit
 alias gca='git commit -v -a'
-compdef _git gca=git-commit
 alias gcm='git commit -m'
-compdef _git gcm=git-commit
 
 alias gco='git checkout'
 compdef _git gco=git-checkout
+alias go='git checkout'
+compdef _git go=git-checkout
 alias gpl='git pull --rebase'
 compdef _git gpl=git-pull
 alias gb='git branch'
