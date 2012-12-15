24e0b8db0cb27c9129731b4e61bf7dd52cec86a5
Teo Ljungberg <teo.ljungberg@gmail.com>
2012-12-15 16:13:36 +0100

    Various vim and zsh changes

--- home/.zsh/completion.zsh
+++ home/.zsh/completion.zsh
@@ -4,4 +4,5 @@ zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 # pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
 zstyle ':completion:*' completer _complete _ignored
+zstyle ':completion:*:*:*:*:*' menu select
 zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-]#)*=01;34=0=01'
