825585a7eda1737fa1185a817366d5aa349dfd05
Teo Ljungberg <teo.ljungberg@gmail.com>
2013-03-29 18:50:26 +0100

    Cleared out some git aliases

--- home/.zsh/applications/git.zsh
+++ home/.zsh/applications/git.zsh
@@ -6,12 +6,12 @@ alias gc='git commit -v'
 alias gca='git commit -v -a'
 alias gcm='git commit -m'
 alias gl="git lg `$git_branch | sed -e 's/(//' -e 's/)//'` -n15"
+alias gs='git status -sb'
+alias gb="git branch"
 
 # with completion
 alias g='git'
 compdef g=git
-alias gs='git status -sb'
-compdef _git gst=git-status
 alias gd='git diff'
 compdef _git gd=git-diff
 alias gp='git push'
@@ -20,5 +20,3 @@ alias gco='git checkout'
 compdef _git gco=git-checkout
 alias go='git checkout'
 compdef _git go=git-checkout
-alias gb='git branch'
-compdef _git gb=git-branch
