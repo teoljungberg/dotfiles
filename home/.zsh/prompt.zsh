055100ae3212a12ebcd6bfeff6e338984c41b3ab
Teo Ljungberg <teo.ljungberg@gmail.com>
2013-04-21 17:26:25 +0200

    leaner prompt

--- home/.zsh/prompt.zsh
+++ home/.zsh/prompt.zsh
@@ -3,7 +3,7 @@
 git_branch() {
   BRANCH_REFS=$(git symbolic-ref HEAD 2>/dev/null) || return
   GIT_BRANCH="${BRANCH_REFS#refs/heads/}"
-  [ -n "$GIT_BRANCH" ] && echo "($GIT_BRANCH) "
+  [ -n "$GIT_BRANCH" ] && echo "$GIT_BRANCH "
 }
 
 function precmd {
