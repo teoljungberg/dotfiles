0c5c20a6465c8a9f7d9863d11d940dd10411bfab
Teo Ljungberg <teo.ljungberg@gmail.com>
2013-05-18 19:52:05 +0200

    The "-n" flag isn't needed

--- home/.zsh/applications/git.zsh
+++ home/.zsh/applications/git.zsh
@@ -5,7 +5,7 @@ alias ga='git add'
 alias gc='git commit -v'
 alias gca='git commit -v -a'
 alias gcm='git commit -m'
-alias gl="git lg `$git_branch | sed -e 's/(//' -e 's/)//'` -n15"
+alias gl="git lg `$git_branch | sed -e 's/(//' -e 's/)//'` -15"
 alias gs='git status -sb'
 alias gb="git branch"
 
