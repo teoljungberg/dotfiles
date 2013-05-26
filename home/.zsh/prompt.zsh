5aa5e8bf3802d8417cb7fecb921c0066e990f577
Teo Ljungberg <teo.ljungberg@gmail.com>
2013-05-26 17:28:36 +0200

    Minimalistic prompt for zsh and tmux

--- home/.zsh/prompt.zsh
+++ home/.zsh/prompt.zsh
@@ -17,7 +17,7 @@ rename_tmux_window_to_current_dir() {
 
 function precmd {
   rename_tmux_window_to_current_dir
-  PROMPT=" %c %{$fg[cyan]%}$(git_branch)%{$reset_color%}%# "
+  PROMPT="%c %{$fg[cyan]%}$(git_branch)%{$reset_color%}%# "
 }
 
 # use solarized-like color for ls
