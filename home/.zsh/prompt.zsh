7527371a02fa1cf3abcd673afda3ed3479f006b5
Teo Ljungberg <teo.ljungberg@gmail.com>
2014-01-27 13:34:43 +0100

    Yellow git branch

--- home/.zsh/prompt.zsh
+++ home/.zsh/prompt.zsh
@@ -3,7 +3,7 @@
 function precmd {
   print -Pn "\e]2;%c\a" # tmux window
   print -Pn "\033]0;%c\007" # iterm tab
-  PROMPT="%c %{$fg[cyan]%}$(git_branch)%{$reset_color%}%# "
+  PROMPT="%c %{$fg[yellow]%}$(git_branch)%{$reset_color%}%# "
 }
 
 # use solarized-like color for ls
