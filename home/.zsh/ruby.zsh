c688aaf08908a1d9638b3488c5d3886e5fc9e1f8
Teo Ljungberg <teo.ljungberg@gmail.com>
2014-02-20 10:47:35 +0100

    Replace chruby with rbenv

--- home/.zsh/ruby.zsh
+++ home/.zsh/ruby.zsh
@@ -1,10 +1,5 @@
-if [ -r /usr/local/share/chruby/chruby.sh ]; then
-  source /usr/local/share/chruby/chruby.sh
-  source /usr/local/share/chruby/auto.sh
-  RUBIES=(~/.rubies/*)
-
-  chruby 2.1
-fi
+# rbenv
+eval "$(rbenv init -)"
 
 # aliases
 alias s='spring'
@@ -16,10 +11,3 @@ export RUBY_HEAP_SLOTS_INCREMENT=1000000
 export RUBY_HEAP_SLOTS_GROWTH_FACTOR=1
 export RUBY_GC_MALLOC_LIMIT=1000000000
 export RUBY_HEAP_FREE_MIN=500000
-
-# share gems installed to ~/.gem/shared between rubies
-# to install a shared gem:
-#       $ gem install bundler -i ~/.gem/shared
-if [ -d ~/.gem/shared ]; then
-  export GEM_PATH="$GEM_PATH:$HOME/.gem/shared"
-fi
