cfe444407f52d4884ed31d610296fa5c57a0160a
Teo Ljungberg <teo.ljungberg@gmail.com>
2014-03-23 11:56:57 +0100

    Break out history configs

--- home/.zsh/config.zsh
+++ home/.zsh/config.zsh
@@ -3,14 +3,6 @@ setopt nobeep
 setopt no_bg_nice # don't nice background tasks
 setopt alwaystoend # when complete from middle, move cursor
 
-setopt append_history # adds history
-setopt extended_history # add timestamps to history
-setopt hist_expire_dups_first
-setopt hist_ignore_all_dups # don't record dupes in history
-HISTFILE=~/.zsh_history
-HISTSIZE=10000
-SAVEHIST=10000
-
 export EDITOR='vim'
 export VISUAL="$EDITOR"
 export LESS="-F -X -R"
