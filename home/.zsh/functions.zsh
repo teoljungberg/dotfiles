84579a77807f1e60f37d7443037233940d01b29b
Teo Ljungberg <teo.ljungberg@gmail.com>
2014-07-21 17:09:43 +0200

    Remove `extract` function

--- home/.zsh/functions.zsh
+++ home/.zsh/functions.zsh
@@ -36,25 +36,6 @@ o() {
 _o() { _files -W /opt -/; }
 compdef _o o
 
-extract() {
-  if [ -f $1 ]; then
-    case $1 in
-      *.tar.bz2)  tar -jxvf $1        ;;
-      *.tar.gz)   tar -zxvf $1        ;;
-      *.bz2)      bzip2 -d $1         ;;
-      *.gz)       gunzip -d $1        ;;
-      *.tar)      tar -xvf $1         ;;
-      *.tgz)      tar -zxvf $1        ;;
-      *.zip)      unzip $1            ;;
-      *.Z)        uncompress $1       ;;
-      *.rar)      unrar x $1            ;;
-      *)          echo "'$1' Error. Unsupported filetype." ;;
-    esac
-  else
-    echo "'$1' is not a valid file"
-  fi
-}
-
 b() {
   if [[ -a Gemfile ]]; then
     bundle exec $*
