42079ddb91c3de7026680bcd918fa8b514c55830
Teo Ljungberg <teo.ljungberg@gmail.com>
2014-08-05 12:16:53 +0200

    Rename tmux window to current directory
    And keep the current command intact

--- home/.zsh/prompt.zsh
+++ home/.zsh/prompt.zsh
@@ -1,8 +1,17 @@
 #!/bin/sh
 
+rename_tmux_window_to_current_dir() {
+  if [ ! -z "$TMUX" ]; then
+    if [ "$PWD" != "$LPWD" ]; then
+      LPWD="$PWD"
+      tmux rename-window $(print -Pn "%c")
+    fi
+  fi
+}
+
 function precmd {
   # set the current directory as the title for tmux window and terminal tab
-  print -Pn "\e]2;%c\a"
+  rename_tmux_window_to_current_dir
   print -Pn "\033]0;%c\007"
   PROMPT="%c %{$fg[yellow]%}$(git_branch)%{$reset_color%}%# "
 }
