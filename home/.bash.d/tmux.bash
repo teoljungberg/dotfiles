444f774c1bb06b265eb7dda37436d62274fa2b42
Teo Ljungberg <teo.ljungberg@gmail.com>
2014-08-08 16:54:02 +0200

    Replace `$HOME` with `~` in `rename_tmux_window_to_current_dir`

--- home/.bash.d/tmux.bash
+++ home/.bash.d/tmux.bash
@@ -2,7 +2,7 @@ rename_tmux_window_to_current_dir() {
   if [[ "$TERM" == "screen-256color" && -n "$TMUX" ]]; then
     if [[ "$PWD" != "$LPWD" ]]; then
       LPWD="$PWD"
-      tmux rename-window ${PWD//*\//}
+      tmux rename-window $(basename "${PWD/#$HOME/~}")
     fi
   fi
 }
