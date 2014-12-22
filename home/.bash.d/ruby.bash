736b3756a49d2c244dd99648c101a381ebe5418e
Teo Ljungberg <teo@teoljungberg.com>
2014-12-22 12:00:39 +0100

    Remove ruby env-variables
    It's unnecessary on RUBY_VERSION >= 2

--- home/.bash.d/ruby.bash
+++ home/.bash.d/ruby.bash
@@ -6,10 +6,3 @@ export PATH="$HOME/.gem/bin:$PATH"
 alias s='spring'
 alias sc='spring rails console'
 alias zomg='omg `echo ${PWD##*/}`'
-
-# FASTER
-export RUBY_GC_HEAP_INIT_SLOTS=1000000
-export RUBY_HEAP_SLOTS_INCREMENT=1000000
-export RUBY_HEAP_SLOTS_GROWTH_FACTOR=1
-export RUBY_GC_MALLOC_LIMIT=1000000000
-export RUBY_HEAP_FREE_MIN=500000
