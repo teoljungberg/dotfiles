7263282009bce8769e5e2d4330286bf5e05e3f0e
Teo Ljungberg <teo@teoljungberg.com>
2015-01-12 10:15:24 +0100

    Remove Postgres.app

--- home/.bash.d/01_path.bash
+++ home/.bash.d/01_path.bash
@@ -6,10 +6,6 @@ if [ -d /usr/local/bin ]; then
   export PATH=$PATH:/usr/local/sbin
 fi
 
-if [ -d /Applications/Postgres.app ]; then
-  export PATH=$PATH:/Applications/Postgres.app/Contents/Versions/9.3/bin
-fi
-
 if [[ -d $HOME/bin ]]; then
   export PATH=$PATH:$HOME/bin
 fi
