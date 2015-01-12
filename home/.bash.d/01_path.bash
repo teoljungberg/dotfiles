4bb58527c2891cd605ab5e28deed72b7a2cb2f55
Teo Ljungberg <teo@teoljungberg.com>
2015-01-12 10:15:46 +0100

    Add nvm and node to PATH

--- home/.bash.d/01_path.bash
+++ home/.bash.d/01_path.bash
@@ -6,6 +6,9 @@ if [ -d /usr/local/bin ]; then
   export PATH=$PATH:/usr/local/sbin
 fi
 
+export PATH="$PATH:/usr/local/lib/node_modules"
+source $(brew --prefix nvm)/nvm.sh
+
 if [[ -d $HOME/bin ]]; then
   export PATH=$PATH:$HOME/bin
 fi
