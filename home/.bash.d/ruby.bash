49c7dfdaed24fdac1f0b4e95fe23ed17c0adf2de
Teo Ljungberg <teo@teoljungberg.com>
2015-01-19 22:48:00 +0100

    Replace `rbenv` with `chruby`

--- home/.bash.d/ruby.bash
+++ home/.bash.d/ruby.bash
@@ -1,6 +1,11 @@
-export GEM_HOME="$HOME/.gem"
-export GEM_PATH="$HOME/.gem"
-export PATH="$HOME/.gem/bin:$PATH"
+if [ -f /usr/local/opt/chruby/share/chruby/chruby.sh ]; then
+  source /usr/local/opt/chruby/share/chruby/chruby.sh
+
+  RUBIES=(~/.rubies/*)
+
+  source /usr/local/opt/chruby/share/chruby/auto.sh
+fi
+
 export PATH=".git/safe/../../bin:$PATH"
 
 # aliases
--- home/.bash_profile
+++ home/.bash_profile
@@ -1,9 +1,5 @@
 . ~/.bashrc
 
-if which rbenv > /dev/null; then
-  eval "$(rbenv init -)"
-fi
-
 if [ -f ~/.bash_profile.local ]; then
   . ~/.bash_profile.local
 fi
