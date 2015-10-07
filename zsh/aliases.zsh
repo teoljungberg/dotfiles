656f219ee8ab84d950f919fb1fbd8e7a8a6306ad
Teo Ljungberg <teo@teoljungberg.com>
2015-10-07 16:09:59 +0200

    Remove unused aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -6,18 +6,11 @@ alias reload='source ~/.zshrc'
 # listing
 alias ls="ls -GF"
 alias l='ls -1'
-alias ..='cd ..'
-alias ...='cd ../..'
-
-# ruby
-alias s='spring'
-alias sc='spring rails console'
 
 # Git aliases
 alias ga='git aa'
 alias gd='git diff'
 alias gdc='git diff --cached'
 alias gc='git commit -v'
-alias gca='git commit -v -a'
 alias gl="git log --oneline -15"
 alias gg="git grep"
