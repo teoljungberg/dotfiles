7ea498983ca593327b19e1922df3d9da1633bd25
Teo Ljungberg <teo@teoljungberg.com>
2015-11-05 11:28:54 +0100

    Remove superfluous comments

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,12 +1,8 @@
-# General aliases
 alias reload='source ~/.zshrc'
-
-# listing
 alias b="bundle exec"
 alias ls="ls -GF"
 alias l='ls -1'
 
-# Git aliases
 alias ga='git aa'
 alias gd='git diff'
 alias gdc='git diff --cached'
