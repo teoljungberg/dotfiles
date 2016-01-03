2fef089c71e0e27885b800095d38ef0d1d84798c
Teo Ljungberg <teo@teoljungberg.com>
2016-01-03 22:55:50 +0100

    Simplify `zsh` history config
    - Append history to the history file.

--- zsh/history.zsh
+++ zsh/history.zsh
@@ -1,7 +1,4 @@
-setopt append_history # adds history
-setopt extended_history # add timestamps to history
-setopt hist_expire_dups_first
-setopt hist_ignore_all_dups # don't record dupes in history
+setopt hist_ignore_all_dups inc_append_history
 HISTFILE=~/.zsh_history
 HISTSIZE=10000
 SAVEHIST=10000
