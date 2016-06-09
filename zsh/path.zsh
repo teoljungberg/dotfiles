3a3532fa22111c7dd7408639e0a8cfa3d1840534
Teo Ljungberg <teo@teoljungberg.com>
2016-06-09 10:43:26 +0200

    Add `git fixup`
    Which leverages [yank] to select the commit we want to `git commit
    --fixup` into.
    
    - Add `$HOME/.bin` to `$PATH`
    
    [yank]: https://github.com/mptre/yank

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -5,6 +5,6 @@ if [ -d /usr/local/Cellar ]; then
   PATH="/usr/local/bin:$PATH"
 fi
 
-if [ -d "$HOME/bin" ]; then
-  PATH="$PATH:$HOME/bin"
+if [ -d "$HOME/.bin" ]; then
+  PATH="$PATH:$HOME/.bin"
 fi
