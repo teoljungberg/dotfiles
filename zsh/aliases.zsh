f96c4d7640745e5a638c43e7c203b5640d4bf743
Teo Ljungberg <teo@teoljungberg.com>
2017-10-31 11:23:24 +0100

    Remove `g*` aliases
    I want to get into the habit of writing `g CMD` rather than using the
    aliases directly. It makes pairing with others easier and less
    confusing for the observer.
    
    Changes:
    - Change `g aa` to `g a` to mimic the old `ga` alias.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,9 +1,3 @@
 alias reload='source ~/.zshrc'
 alias b="bundle exec"
 alias j="jobs"
-
-alias ga='git aa'
-alias gd='git diff'
-alias gdc='git diff --cached'
-alias gc='git commit -v'
-alias gl="git log --oneline -15"
