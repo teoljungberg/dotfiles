709e7e52b0fd91d94d8f829b4dd0c81e0fd772e5
Teo Ljungberg <teo@teoljungberg.com>
2018-01-03 09:58:51 +0100

    Remove naming the current tab after the `pwd`

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -8,9 +8,7 @@ rename_tmux_window_to_current_dir() {
 }
 
 function precmd {
-  # set the current directory as the title for tmux window and terminal tab
   rename_tmux_window_to_current_dir
-  print -Pn "\033]0;%c\007"
   PROMPT="%c %{$fg[yellow]%}$(git_branch)%{$reset_color%}%# "
 }
 
