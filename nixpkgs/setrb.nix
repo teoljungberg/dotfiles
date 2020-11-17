0217a522a007289303410a342cc5a982eac32097
Teo Ljungberg <teo@teoljungberg.com>
2020-11-17 20:25:17 +0100

    Bump setrb

--- nixpkgs/setrb.nix
+++ nixpkgs/setrb.nix
@@ -3,11 +3,11 @@
 
 stdenv.mkDerivation {
   pname = "setrb";
-  version = "0.3";
+  version = "0.3.1";
 
   src = fetchzip {
-    url = "https://mike-burns.com/project/setrb/setrb-0.3.tar.gz";
-    sha256 = "0jgigkvlz1c64dx6rha6jmxaw7s85sydsmvh2njhn5359yax4ilk";
+    url = "https://mike-burns.com/project/setrb/setrb-0.3.1.tar.gz";
+    sha256 = "0wm7c3248c21qr8i7a0491i272qcyj1b338fxiw8r72ydnj5fxdj";
   };
 
   meta = {
