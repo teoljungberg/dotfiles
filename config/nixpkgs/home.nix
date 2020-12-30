5998b02ecef7a1243a65d082e0917edd5885f3e8
Teo Ljungberg <teo@teoljungberg.com>
2020-12-30 10:07:10 +0100

    Install comma

--- config/nixpkgs/home.nix
+++ config/nixpkgs/home.nix
@@ -3,8 +3,10 @@ let
   callPackage = pkgs.callPackage;
 
   asdf = callPackage /Users/teo/src/dotfiles/nixpkgs/asdf.nix { };
+  comma = callPackage /Users/teo/src/dotfiles/nixpkgs/comma.nix {};
   setrb = callPackage /Users/teo/src/dotfiles/nixpkgs/setrb.nix { };
-  vimFromSource = callPackage /Users/teo/src/dotfiles/nixpkgs/vim_from_source.nix { };
+  vimFromSource =
+    callPackage /Users/teo/src/dotfiles/nixpkgs/vim_from_source.nix { };
 in {
   programs.home-manager.enable = true;
   home.stateVersion = "20.03";
@@ -14,6 +16,7 @@ in {
     autoconf
     automake
     coreutils-prefixed
+    comma
     fzf
     git
     gitAndTools.gh
