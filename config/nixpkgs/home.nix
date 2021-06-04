458327543fdd996ec0f7420c5d00edd5fc0e350a
Teo Ljungberg <teo@teoljungberg.com>
2021-06-04 09:21:54 +0200

    Replace nixpkgs-teo with import

--- config/nixpkgs/home.nix
+++ config/nixpkgs/home.nix
@@ -1,12 +1,16 @@
 { pkgs, ... }:
 
+# https://status.nixos.org
+# with import
+#   (fetchTarball "https://github.com/NixOS/nixpkgs/archive/COMMIT_SHA.tar.gz")
+#   { };
+
 let
   user = import ./../../nixpkgs/user.nix { };
 
   asdf = pkgs.callPackage "${user.user.directory}/.nixpkgs/asdf.nix" { };
   comma = pkgs.callPackage "${user.user.directory}/.nixpkgs/comma.nix" { };
   setrb = pkgs.callPackage "${user.user.directory}/.nixpkgs/setrb.nix" { };
-  nixpkgs-teo = import "${user.user.directory}/src/nixpkgs" { };
 in {
   programs.home-manager.enable = true;
 
