1af06e83804e82974c4a4c8a0af1961105de351d
Teo Ljungberg <teo@teoljungberg.com>
2021-06-24 10:47:14 +0200

    Derive from build environment

--- nixpkgs/ruby-install.nix
+++ nixpkgs/ruby-install.nix
@@ -2,16 +2,8 @@
 , fetchzip ? (import <nixpkgs> { }).fetchzip, pkgs ? (import <nixpkgs> { }), ...
 }:
 
-stdenv.mkDerivation {
-  pname = "ruby-install";
-  version = "0.8.1";
-
-  src = fetchzip {
-    url = "https://github.com/postmodern/ruby-install/archive/v0.8.1.tar.gz";
-    sha256 = "0d92mdjg7k20sx6wd2ngbblq9m8njv45khnlf8ilc2034mz287kx";
-  };
-
-  buildInputs = [
+let
+  paths = [
     pkgs.gcc
     pkgs.gdbm
     pkgs.gnumake
@@ -23,6 +15,21 @@ stdenv.mkDerivation {
     pkgs.readline
     pkgs.zlib
   ];
+  env = pkgs.buildEnv {
+    name = "ruby-install-env";
+    paths = paths;
+    extraOutputsToInstall = [ "bin" "include" "lib" ];
+  };
+in stdenv.mkDerivation {
+  pname = "ruby-install";
+  version = "0.8.1";
+
+  src = fetchzip {
+    url = "https://github.com/postmodern/ruby-install/archive/v0.8.1.tar.gz";
+    sha256 = "0d92mdjg7k20sx6wd2ngbblq9m8njv45khnlf8ilc2034mz287kx";
+  };
+
+  buildInputs = paths;
 
   buildPhase = ''
     true
@@ -31,11 +38,10 @@ stdenv.mkDerivation {
   installPhase = ''
     make install PREFIX=$out/
     wrapProgram $out/bin/ruby-install \
-      --set PKG_CONFIG_PATH "${pkgs.openssl.dev}/lib/pkgconfig" \
-      --set CFLAGS "-I${pkgs.libyaml}/include -I${pkgs.ncurses}/include -I${pkgs.openssl.dev}/include -I${pkgs.readline}/include -I${pkgs.zlib}/include" \
-      --set LDFLAGS "-L${pkgs.libyaml}/lib -L${pkgs.ncurses}/lib -L${pkgs.openssl.dev}/lib -L${pkgs.readline}/lib -L${pkgs.zlib}/lib" \
-      --prefix PATH : "${pkgs.gnumake}/bin" \
-      --prefix PATH : "${pkgs.gcc}/bin"
+      --set PKG_CONFIG_PATH "${env}/lib/pkgconfig" \
+      --set CFLAGS "-I${env}/include" \
+      --set LDFLAGS "-L${env}/lib" \
+      --prefix PATH : "${env}/bin"
   '';
 
   meta = with lib; {
