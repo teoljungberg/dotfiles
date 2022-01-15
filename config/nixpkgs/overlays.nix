e47e90cc3174d97a33b00f908b02097cc91012ce
Teo Ljungberg <teo@teoljungberg.com>
2022-01-15 22:47:33 +0100

    Only one override closure

--- config/nixpkgs/overlays.nix
+++ config/nixpkgs/overlays.nix
@@ -1,19 +1,21 @@
 [
-  (self: super: { comma = super.callPackage ./../../nixpkgs/comma.nix { }; })
-  (self: super: { diff-highlight = super.callPackage ./../../nixpkgs/diff-highlight.nix { }; })
-  (self: super: { gitsh = super.callPackage ./../../nixpkgs/gitsh.nix { }; })
-  (self: super: {
-    heroku = super.heroku.overrideAttrs (
-      oldAttrs: rec {
-        version = "7.59.2";
+  (
+    self: super: {
+      comma = super.callPackage ./../../nixpkgs/comma.nix { };
+      diff-highlight = super.callPackage ./../../nixpkgs/diff-highlight.nix { };
+      gitsh = super.callPackage ./../../nixpkgs/gitsh.nix { };
+      heroku = super.heroku.overrideAttrs (
+        oldAttrs: rec {
+          version = "7.59.2";
 
-        src = fetchTarball {
-          url = "https://cli-assets.heroku.com/heroku-v${version}/heroku-v${version}.tar.xz";
-          sha256 = "0qj651gikw8c80pn6z44pzm7r5hhc58cgdvgbqlqwz5bw76ii88f";
-        };
-      }
-    );
-  })
-  (self: super: { ruby-install = super.callPackage ./../../nixpkgs/ruby-install.nix { }; })
-  (self: super: { setrb = super.callPackage ./../../nixpkgs/setrb.nix { }; })
+          src = fetchTarball {
+            url = "https://cli-assets.heroku.com/heroku-v${version}/heroku-v${version}.tar.xz";
+            sha256 = "0qj651gikw8c80pn6z44pzm7r5hhc58cgdvgbqlqwz5bw76ii88f";
+          };
+        }
+      );
+      ruby-install = super.callPackage ./../../nixpkgs/ruby-install.nix { };
+      setrb = super.callPackage ./../../nixpkgs/setrb.nix { };
+    }
+  )
 ]
