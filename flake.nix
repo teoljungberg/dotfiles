fb44d84d657a75cd16fe15a6ec04a9019967532e
Teo Ljungberg <teo@teoljungberg.com>
2022-03-18 09:09:19 +0000

    Consistency

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = github:NixOS/nixpkgs/nixpkgs-unstable;
-    darwin.url = "github:lnl7/nix-darwin/master";
+    darwin.url = github:lnl7/nix-darwin/master;
     darwin.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
