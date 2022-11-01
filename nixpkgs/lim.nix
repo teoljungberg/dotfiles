7d21a4d2fe40ae699d72bfad3e7725fd4219c951
Teo Ljungberg <teo@teoljungberg.com>
2022-11-01 09:47:40 +0100

    Allow building lim on different versions of vim(1)

--- nixpkgs/lim.nix
+++ nixpkgs/lim.nix
@@ -1,10 +1,22 @@
 { pkgs ? import <nixpkgs> { }
+, lib ? pkgs.lib
 , runCommand ? pkgs.runCommand
 , vim ? pkgs.vim
 }:
 
+with lib;
+
+let
+  src =
+    if versionAtLeast vim.version "9.0" then
+      "${vim}/share/vim/vim90/macros/less.sh"
+    else if versionAtLeast vim.version "8.2" then
+      "${vim}/share/vim/vim82/macros/less.sh"
+    else
+      builtins.throw "No less macro found inside ${vim}";
+in
 runCommand "lim" { } ''
   mkdir -p $out/bin
 
-  ln -s ${vim}/share/vim/vim90/macros/less.sh $out/bin/lim
+  ln -s ${src} $out/bin/lim
 ''
