623bf406545ce84025e0b32bb68dde6da726aa13
Teo Ljungberg <teo@teoljungberg.com>
2022-11-01 12:18:35 +0100

    Add safe-guard against missing script for diff-highlight

--- nixpkgs/diff-highlight.nix
+++ nixpkgs/diff-highlight.nix
@@ -3,10 +3,15 @@
 , git ? pkgs.git
 }:
 
+let
+  src =
+    if builtins.pathExists "${git}/share/git/contrib/diff-highlight" then
+      "${git}/share/git/contrib/diff-highlight/diff-highlight"
+    else
+      builtins.throw "No diff-highlight found inside ${git}";
+in
 runCommand "diff-highlight" { } ''
   mkdir -p $out/bin
 
-  ln -s \
-    ${git}/share/git/contrib/diff-highlight/diff-highlight \
-    $out/bin/diff-highlight
+  ln -s ${src} $out/bin/diff-highlight
 ''
