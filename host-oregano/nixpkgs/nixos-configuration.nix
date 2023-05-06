140da3054e2d562c07d35800f6165c49fda3d778
Teo Ljungberg <teo@teoljungberg.com>
2023-05-06 16:01:15 +0200

    Configure ip forwarding for tailscale(1)

--- host-oregano/nixpkgs/nixos-configuration.nix
+++ host-oregano/nixpkgs/nixos-configuration.nix
@@ -15,6 +15,7 @@ in rec
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernel.sysctl."net.ipv4.ip_forward" = 1;
 
   networking = {
     hostName = "oregano";
