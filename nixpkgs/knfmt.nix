5c66fea31d9b105d711895dc8ee8eb31bbe03905
Teo Ljungberg <teo@teoljungberg.com>
2024-01-29 14:37:45 +0100

    Use the extracted version for the knfmt(1) derivation

--- nixpkgs/knfmt.nix
+++ nixpkgs/knfmt.nix
@@ -1,7 +1,7 @@
 {
   pkgs ? import <nixpkgs> {},
   stdenv ? pkgs.stdenv,
-  lib ? pkgs.lib
+  lib ? pkgs.lib,
 }: let
   version = "4.3.0";
 in
@@ -10,7 +10,7 @@ in
     inherit version;
 
     src = fetchTarball {
-      url = "https://github.com/mptre/knfmt/releases/download/v4.3.0/knfmt-4.3.0.tar.gz";
+      url = "https://github.com/mptre/knfmt/releases/download/v${version}/knfmt-${version}.tar.gz";
       sha256 = "0hwq5s6r46n5ngjz7ljcgc7p4mcssvhc54zva7fslg6x7zgwd3b3";
     };
 
