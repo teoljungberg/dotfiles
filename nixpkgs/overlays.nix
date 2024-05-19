db6e7d0df5f32da79921352ec5a4cae602ffc353
Teo Ljungberg <teo@teoljungberg.com>
2024-05-19 16:49:59 +0200

    Extract tmux from overlays.nix

--- nixpkgs/overlays.nix
+++ nixpkgs/overlays.nix
@@ -7,20 +7,7 @@
       lim = prev.callPackage ./lim.nix {pkgs = prev;};
       ripper-tags = prev.callPackage ./ripper-tags {pkgs = prev;};
       ruby-install = prev.callPackage ./ruby-install.nix {pkgs = prev;};
-      tmux = prev.tmux.overrideAttrs (
-        let
-          tmuxVersion = "3.4";
-        in
-          oldAttrs: {
-            version = tmuxVersion;
-            patches = [];
-
-            src = fetchTarball {
-              url = "https://github.com/tmux/tmux/releases/download/${tmuxVersion}/tmux-${tmuxVersion}.tar.gz";
-              sha256 = "0dk15lcs0d2p9xp2g3f8ca3fldd37ib4ck9spzb4v8q0gwmrpw4n";
-            };
-          }
-      );
+      tmux = prev.callPackage ./tmux.nix {pkgs = prev;};
     }
   )
 ]
