bdc844d6fff253b5016b181d51ed33e3b861df97
Teo Ljungberg <teo@teoljungberg.com>
2024-09-02 10:14:34 +0200

    Upgrade heroku to v9.2.0

--- nixpkgs/heroku-aarch64-darwin.nix
+++ nixpkgs/heroku-aarch64-darwin.nix
@@ -2,12 +2,11 @@
 if pkgs.stdenv.hostPlatform.system != "aarch64-darwin"
 then throw "Unsupported platform"
 else let
-  version = "8.11.5";
-  gitSha = "df5cd30";
+  version = "9.2.0";
   src = fetchTarball {
-    # https://github.com/heroku/homebrew-brew/commit/3b6ca6a88b49e47a0fd7625eff10a2475ac7e1a4
-    url = "https://cli-assets.heroku.com/versions/${version}/${gitSha}/heroku-v${version}-${gitSha}-darwin-x64.tar.xz";
-    sha256 = "1g6lbxgff3d3n05dwrdlm94kf5jsim6jxdxzf1qmhzq8agnc1wd0";
+    # https://github.com/heroku/homebrew-brew/commit/e8b2502a7209c5ea3089c63f9e3f409c7951ba49
+    url = "https://cli-assets.heroku.com/versions/9.2.0/2aa043a/heroku-v9.2.0-2aa043a-darwin-arm64.tar.xz";
+    sha256 = "14cwcisikq5ry2646j7qx3s60qkx44ymgbkj010s5nn051lh2q4a";
   };
 in
   pkgs.heroku.overrideAttrs (oldAttrs: {
