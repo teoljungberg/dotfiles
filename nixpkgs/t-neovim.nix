332385365f245ba693c8017b2c1899f240a73f6b
Teo Ljungberg <teo@teoljungberg.com>
2024-11-23 15:35:47 +0100

    Refactor to bundle to avoid nested if's

--- nixpkgs/t-neovim.nix
+++ nixpkgs/t-neovim.nix
@@ -7,27 +7,31 @@
 let
   inherit (pkgs) stdenv lib;
 
-  version = if useNightly then "nightly" else "0.10.2";
-
-  src =
+  bundle =
     if useNightly then
       if stdenv.hostPlatform.system == "aarch64-darwin" then
-        pkgs.fetchurl {
-          url = "https://github.com/neovim/neovim/releases/download/nightly/nvim-macos-x86_64.tar.gz";
-          sha256 = lib.fakeSha256;
+        {
+          version = "nightly";
+          src = pkgs.fetchurl {
+            url = "https://github.com/neovim/neovim/releases/download/nightly/nvim-macos-x86_64.tar.gz";
+            sha256 = lib.fakeSha256;
+          };
         }
       else
         throw "Unsupported platform for nightly"
     else if stdenv.hostPlatform.system == "aarch64-darwin" then
-      pkgs.fetchurl {
-        url = "https://github.com/neovim/neovim/releases/download/v${version}/nvim-macos-arm64.tar.gz";
-        sha256 = "zT4ul/7ihcQQFs7OUeZ+gXTfHcV3uJvAQGOyBozNrqA=";
+      {
+        version = "0.10.2";
+        src = pkgs.fetchurl {
+          url = "https://github.com/neovim/neovim/releases/download/v0.10.2/nvim-macos-arm64.tar.gz";
+          sha256 = "zT4ul/7ihcQQFs7OUeZ+gXTfHcV3uJvAQGOyBozNrqA=";
+        };
       }
     else
-      throw "Unsupported platform for ${version}";
+      throw "Unsupported platform";
 in
 stdenv.mkDerivation {
-  inherit version src;
+  inherit (bundle) version src;
   name = "neovim";
 
   installPhase =
