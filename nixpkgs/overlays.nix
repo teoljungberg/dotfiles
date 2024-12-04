e4f54c3154cbf9deea2a33579e2cff63b8aa9e8d
Teo Ljungberg <teo@teoljungberg.com>
2024-12-04 14:25:35 +0100

    Remove heroku derivation

--- nixpkgs/overlays.nix
+++ nixpkgs/overlays.nix
@@ -1,7 +1,6 @@
 [
   (final: prev: {
     hammerspoon = prev.callPackage ./hammerspoon.nix { pkgs = prev; };
-    heroku-aarch64-darwin = prev.callPackage ./heroku-aarch64-darwin.nix { pkgs = prev; };
     knfmt = prev.callPackage ./knfmt.nix { pkgs = prev; };
     lim = prev.callPackage ./lim.nix { pkgs = prev; };
     ripper-tags = prev.callPackage ./ripper-tags { pkgs = prev; };
