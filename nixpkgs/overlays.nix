c7d8f637bedbdb54a95405b6cc33cac4b8a3c5e9
Teo Ljungberg <teo@teoljungberg.com>
2025-04-09 09:13:29 +0200

    Add standardrb 1.49.0

--- nixpkgs/overlays.nix
+++ nixpkgs/overlays.nix
@@ -3,5 +3,6 @@
     hammerspoon = prev.callPackage ./hammerspoon.nix { pkgs = prev; };
     knfmt = prev.callPackage ./knfmt.nix { pkgs = prev; };
     ripper-tags = prev.callPackage ./ripper-tags { pkgs = prev; };
+    standard = prev.callPackage ./standard { pkgs = prev; };
   })
 ]
