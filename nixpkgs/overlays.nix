a9f717fcd418597155636649f932a3cdccfe20f7
Teo Ljungberg <teo@teoljungberg.com>
2026-01-06 14:49:19 +0100

    Remove setrb(1)

--- nixpkgs/overlays.nix
+++ nixpkgs/overlays.nix
@@ -4,6 +4,5 @@
     knfmt = prev.callPackage ./knfmt.nix { pkgs = prev; };
     nnvim = prev.callPackage ./nnvim.nix { pkgs = prev; };
     ripper-tags = prev.callPackage ./ripper-tags { pkgs = prev; };
-    setrb = prev.callPackage ./setrb.nix { pkgs = prev; };
   })
 ]
